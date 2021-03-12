0561e584b77bca5d92642661accb84e555fc6a65
matiasHeber <mheber@digitalhouse.com>
2021-03-12 16:30:56 -0300

    Primer proyecto de React

---
+++ Modulo 1/mi-primer-proyecto/src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+     <h1>Este es un proyecto hecho con React</h1>
+     <p>Tener en cuenta que todo lo escribamos acá es el contenido que vemos despues en la pagina</p>
+      <ul>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+        <li>Item</li>
+      </ul>
+      <footer></footer>
+    </div>
+
+
+  );
+}
+
+export default App;
